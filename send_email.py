9506e0c30827748c39a3e22b2700e05d71fb22c7
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2024-12-03 10:48:15 +0100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -2,7 +2,7 @@ import smtplib
 
 # Funkcja wysyłania wiadomości e-mail
 def send_email():
-    email = "kruszynskibartosz1@gmail.com"
+    email = "mail"
     recipient_email = "bartosz.kruszynski@student.put.poznan.pl"
     subject = "Zaraz wszystko wybuchnie"
     message = "O nie"
@@ -11,7 +11,7 @@ def send_email():
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login(email, "tdlndbvuxmtocihi")
+    server.login(email, "login")
     server.sendmail(email, recipient_email, text)
     server.quit()
     print("Wiadomość e-mail wysłana!")
